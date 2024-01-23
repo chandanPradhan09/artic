1180ea2198b9515ae06c12a1b0125e5707b001b4
Chandan Pradhan <chandan09112001@gmail.com>
2024-01-23 22:05:47 +0530

    Setup SignIn Page and a SonarTost

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,5 +1,96 @@
-const signIn = () => {
-    return <div>SignIn</div>;
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner"
+
+
+const formSchema = z.object({
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+const SignIn = () => {
+
+    const defaultFormValues = {
+        email: "",
+        password: "",
+    };
+
+    const toTitleCase = (str) =>
+        str.replace(/\b\w/g, (char) => char.toUpperCase());
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: defaultFormValues,
+    });
+
+    const onSubmit = (values) => {
+        toast("Sign In Successful",{
+            position:"top-right",
+            duration:2000,
+            icon:"👏",
+            description:values.email,
+        })
+        console.log(values)
+    };
+
+    return (
+        <div className="p-4 rounded-xl shadow-md space-y-4">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4 p-4"
+                >
+                    {Object.keys(defaultFormValues).map((key) => (
+                        <FormField
+                            key={key}
+                            control={form.control}
+                            name={key}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="text-lg font-semibold">
+                                        {toTitleCase(key)}
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            className="w-full p-2 border rounded"
+                                            type={
+                                                key === "email"
+                                                    ? "email"
+                                                    : "password"
+                                            }
+                                            placeholder={toTitleCase(key)}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    ))}
+                    <Button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </Form>
+        </div>
+    );
 };
 
-export default signIn;
+export default SignIn;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
                         <NavBar />
                         {children}
                     </main>
+                    <Toaster />
                 </ThemeProvider>
             </body>
         </html>
